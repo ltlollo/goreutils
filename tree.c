fb159eb977a6f04d75f145882e2d29a5866709cb
L <ltlollo.it@gmail.com>
2017-01-09 08:07:57 +0100

    show hidden

--- tree.c
+++ tree.c
@@ -7,18 +7,20 @@
 #include <string.h>
 #include <sys/types.h>
 
-static void show(size_t, char *);
+static void show(size_t, char *, int);
+static int filt(const char *, int);
 
 int
 main(int argc, char *argv[]) {
-    char *dn = argc - 1 ? argv[1] : (char *)".";
+    char *dn = argc - 1 > 0 ? argv[1] : (char *)".";
+    int hide = argc - 2 > 0 ? strcmp(argv[2], "-h") : 1;
     (void)printf("%s\n", dn);
-    show(0, dn);
+    show(0, dn, hide);
     return 0;
 }
 
 static void
-show(size_t depth, char *dn) {
+show(size_t depth, char *dn, int hide) {
     static char *dnbuf = NULL;
     struct dirent *en0, *en1;
     DIR *dir;
@@ -31,7 +33,7 @@ show(size_t depth, char *dn) {
 	}
     do {
         en0 = readdir(dir);
-    } while (en0 && en0->d_name[0] == '.');
+    } while (en0 && filt(en0->d_name, hide));
     if (en0 == NULL) {
         (void)closedir(dir);
         return;
@@ -39,7 +41,7 @@ show(size_t depth, char *dn) {
 	do {
         do {
 			en1 = readdir(dir);
-		} while (en1 && en1->d_name[0] == '.');
+		} while (en1 && filt(en1->d_name, hide));
         for (i = 0; i < depth; ++i) {
             (void)printf("│   ");
         }
@@ -50,9 +52,22 @@ show(size_t depth, char *dn) {
                 err(1, "realloc");
 			}
             (void)sprintf(subdn, "%s/%s", dn, en0->d_name);
-            show(depth + 1, subdn);
+            show(depth + 1, subdn, hide);
 		}
     } while ((en0 = en1));
 	(void)closedir(dir);
 }
 
+static int
+filt(const char *s, int hide) {
+	if (hide) {
+		return *s == '.';
+	}
+    if (*s == '.' && *(s + 1) == '\0') {
+        return 1;
+	}
+    if (*s == '.' && *(s + 1) == '.' && *(s + 2) == '\0') {
+        return 1;
+	}
+	return 0;
+}
